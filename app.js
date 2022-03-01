25e19e772133dab45d0490578bb2e5bfb3265bd5
Prateek Shukla <53486253+Intangible-pg18@users.noreply.github.com>
2022-03-01 09:35:36 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,54 +1,55 @@
-const p1Button = document.querySelector('#p1Button');
-const p2Button = document.querySelector('#p2Button');
-const p1Vis = document.querySelector('#p1Vis');
-const p2Vis = document.querySelector('#p2Vis');
-const resetButton = document.querySelector('#reset');
-const winningScoreSelect = document.querySelector('#playto');
-
-let p1Score = 0;
-let p2Score = 0;
-let winningScore = 5;
-let isGameOver = false;
-
-p1Button.addEventListener('click',function() {
-    if(!isGameOver)
-    {
-        ++p1Score;
-        if(p1Score === winningScore)
-        { isGameOver = true;
-         p1Vis.classList.add('winner');
-         p2Vis.classList.add('loser');
-        }
-        p1Vis.textContent = p1Score;
-    }
-})
-
-p2Button.addEventListener('click',function() {
-    if(!isGameOver)
-    {
-        ++p2Score;
-        if(p2Score === winningScore)
-        { isGameOver = true;
-         p2Vis.classList.add('winner');
-         p1Vis.classList.add('loser');
-        }
-        p2Vis.textContent = p2Score;
-    }
-})
-
-function reset (){
-    isGameOver = false;
-    p1Score = 0;
-    p2Score = 0;
-    p1Vis.textContent = 0;
-    p2Vis.textContent = 0;
-    p1Vis.classList.remove('winner','loser');
-    p2Vis.classList.remove('winner','loser');
+const p1 = {
+    score: 0,
+    button: document.querySelector('#p1Button'),
+    display: document.querySelector('#p1Display')
+}
+const p2 = {
+    score: 0,
+    button: document.querySelector('#p2Button'),
+    display: document.querySelector('#p2Display')
 }
 
-winningScoreSelect.addEventListener('change',function(){
+const resetButton = document.querySelector('#reset');
+const winningScoreSelect = document.querySelector('#playto');
+let winningScore = 3;
+let isGameOver = false;
+
+function updateScores(player, opponent) {
+    if (!isGameOver) {
+        player.score += 1;
+        if (player.score === winningScore) {
+            isGameOver = true;
+            player.display.classList.add('has-text-success');
+            opponent.display.classList.add('has-text-danger');
+            player.button.disabled = true;
+            opponent.button.disabled = true;
+        }
+        player.display.textContent = player.score;
+    }
+}
+
+
+p1.button.addEventListener('click', function () {
+    updateScores(p1, p2)
+})
+p2.button.addEventListener('click', function () {
+    updateScores(p2, p1)
+})
+
+
+winningScoreSelect.addEventListener('change', function () {
     winningScore = parseInt(this.value);
     reset();
 })
-resetButton.addEventListener('click',reset)
 
+resetButton.addEventListener('click', reset)
+
+function reset() {
+    isGameOver = false;
+    for (let p of [p1, p2]) {
+        p.score = 0;
+        p.display.textContent = 0;
+        p.display.classList.remove('has-text-success', 'has-text-danger');
+        p.button.disabled = false;
+    }
+}
